2564ee8628d20c5e202235d41e06c9eee7024a08
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-01-04 10:16:12 +0100

    :tada: NEW: added auto-install script

---
+++ scripts/install.js
@@ -0,0 +1,39 @@
+//! Please don't change anything here.
+const { exec } = require("child_process");
+
+const clientDir = "./client";
+const serverDir = "./server";
+
+const mainInstall = `npm install`;
+const serverInstall = `cd ${serverDir} && npm install`;
+const clientInstall = `cd ${clientDir} && npm install`;
+
+async function install() {
+  console.log(
+    "Please hold tight, I'm installing the required dependencies! (This can take several minutes)\n"
+  );
+
+  exec(mainInstall, (e) => {
+    if (e) {
+      console.error(e);
+    } else {
+      console.log("Installed 'main' dependencies");
+    }
+  });
+  exec(serverInstall, (e) => {
+    if (e) {
+      console.error(e);
+    } else {
+      console.log("Installed 'server' dependencies");
+    }
+  });
+  exec(clientInstall, (e) => {
+    if (e) {
+      console.error(e);
+    } else {
+      console.log("Installed 'client' dependencies");
+    }
+  });
+}
+
+install();
--- server/config.example.ts
+++ server/config.example.ts
@@ -1,6 +1,6 @@
 const config = {
   port: 3030,
-  clientUrl: "http://localhost:3000",
+  clientUrl: "http://localhost:5000",
   host: "localhost",
   user: "root",
   password: "admin",
