aa614b75b3b69695d32fde9d651d7ac015a025fa
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-02-06 11:12:26 +0100

    :hammer: chore: version bump

---
+++ scripts/auto-update.js
@@ -0,0 +1,25 @@
+//! Only gets run when the CAD gets detected as out of date
+const { exec } = require("child_process");
+
+const GIT_PULL_STRING = "git pull origin main";
+
+function autoUpdate() {
+  console.log("Trying to auto update");
+
+  exec(GIT_PULL_STRING, (err) => {
+    if (err) {
+      console.error("Tried to auto-update but didn't work, please manually update the CAD");
+      return;
+    }
+
+    // CAD is already up to date
+    if (out.includes("Already up to date")) return console.log("CAD is already up to date!");
+
+    console.log("Fetching new code..");
+    console.log("Updating dependencies..\n");
+
+    require("./install");
+  });
+}
+
+autoUpdate();
