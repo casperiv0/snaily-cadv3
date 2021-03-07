858cd87911512451ff2195924e0209ba31aee104
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-03-07 14:50:38 +0100

    :hammer: chore: updated dependencies

--- scripts/install.js
+++ scripts/install.js
@@ -1,39 +1,48 @@
 //! Please don't change anything here.
 const { exec } = require("child_process");
+const fs = require("fs")
+const path = require("path")
 
 const clientDir = "./client";
 const serverDir = "./server";
 
-const mainInstall = `npm install`;
-const serverInstall = `cd ${serverDir} && npm install`;
-const clientInstall = `cd ${clientDir} && npm install`;
+const mainInstall  = async () => await nodeModulesExist(".") ? "npm ci" : "npm install";
+const serverInstall = async () => `cd ${serverDir} && ${await nodeModulesExist("./server") ? "npm ci" : "npm install"}`;
+const clientInstall = async () => `cd ${clientDir} && ${await nodeModulesExist("./client") ? "npm ci" : "npm install"}`;
 
 async function install() {
   console.log(
     "Please hold tight, I'm installing the required dependencies! (This can take several minutes)\n"
   );
+  const main = await mainInstall()
+  const server = await serverInstall()
+  const client = await clientInstall()
 
-  exec(mainInstall, (e) => {
+  exec(main, (e) => {
     if (e) {
       console.error(e);
     } else {
       console.log("Installed 'main' dependencies");
     }
   });
-  exec(serverInstall, (e) => {
+  exec(server, (e) => {
     if (e) {
       console.error(e);
     } else {
       console.log("Installed 'server' dependencies");
     }
   });
-  exec(clientInstall, (e) => {
+  exec(client, (e) => {
     if (e) {
       console.error(e);
     } else {
       console.log("Installed 'client' dependencies");
     }
   });
+}
+
+async function nodeModulesExist(p) {
+  return fs.existsSync(path.resolve(`${p}/node_modules`))
 }
 
 install();
