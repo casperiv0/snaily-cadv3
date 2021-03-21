bd1c5fb5e9243550449bfaec1c5896404658eb15
bound2 <9380102+bound2@users.noreply.github.com>
2021-03-21 20:17:29 +0200

    fix lint issues

--- server/src/routes/monitoring/health.ts
+++ server/src/routes/monitoring/health.ts
@@ -1,26 +1,23 @@
 import { Response, Router } from "express";
-import { processQuery } from "../../lib/database"
+import { processQuery } from "../../lib/database";
 import Logger from "../../lib/Logger";
 import IRequest from "../../interfaces/IRequest";
 
 const router: Router = Router();
 
-router.get(
-  "/liveness",
-  async (_: IRequest, res: Response) => {
-    let code = 200;
-    let status = "UP";
+router.get("/liveness", async (_: IRequest, res: Response) => {
+  let code = 200;
+  let status = "UP";
 
-    try {
-      await processQuery("SELECT 1");
-    } catch (err) {
-      code = 500;
-      status = "DOWN";
-      Logger.log("ERROR", `Liveness failed: ${err.message}`);
-    }
+  try {
+    await processQuery("SELECT 1");
+  } catch (err) {
+    code = 500;
+    status = "DOWN";
+    Logger.log("ERROR", `Liveness failed: ${err.message}`);
+  }
 
-    return res.status(code).json({ status });
-  },
-);
+  return res.status(code).json({ status });
+});
 
 export default router;
