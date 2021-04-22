b6b7e95a6760d31cbc673ef53ec2f684fe505c1c
Casper <casper.iversen2@icloud.com>
2021-04-22 14:36:43 +0200

    :hammer: chore: minor changes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -12,7 +12,7 @@ const NotFound = () => {
         <h1>404</h1>
         <p>Whoops! That page could not be found</p>
 
-        <Link href="/">
+        <Link href="/citizen">
           <a className="btn btn-dark p-2 px-4 fs-5 mt-2">Return Home</a>
         </Link>
       </div>
